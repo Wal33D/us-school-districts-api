dfb1317ff66ba40f5864534fbf997d28f486f782
Wal33D <aquataze@yahoo.com>
2025-08-11 00:32:07 -0400

    increase ecosystem files max memory

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'us-school-districts-api',
       script: 'dist/server.js',
       exec_mode: 'fork',
-      max_memory_restart: '120M',
+      max_memory_restart: '200M',
       autorestart: true,
       watch: false,
       max_restarts: 10,
