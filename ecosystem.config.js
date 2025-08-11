fde298c09d951f89a418030558598f4b6815572f
Wal33D <aquataze@yahoo.com>
2025-08-11 00:34:14 -0400

    400M max

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'us-school-districts-api',
       script: 'dist/server.js',
       exec_mode: 'fork',
-      max_memory_restart: '200M',
+      max_memory_restart: '400M',
       autorestart: true,
       watch: false,
       max_restarts: 10,
